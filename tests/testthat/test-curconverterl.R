b54155a3fabcbe1beebcf1d8695b7adbb68e1c32
johndharrison <johndharrison0@gmail.com>
2016-10-18 14:43:11 -0700

    Test was failing on check but passing with devtools::test. Refactored.

--- tests/testthat/test-curconverterl.R
+++ tests/testthat/test-curconverterl.R
@@ -1,20 +1,29 @@
 context("cURL processing")
 test_that("straighten works", {
-
-  expect_that(all(c(8L, 6L, 6L, 6L, 4L, 5L, 6L, 5L, 6L, 6L) ==
-                    sapply(
-                      sapply(
-                        vapply(list.files(
-                          system.file("extdata",package="curlconverter"),
-                          full.names=TRUE, pattern = "curl"
-                        ),
-                        readLines, character(1), warn=FALSE
-                        ),
-                        straighten
-                      ),
-                      length)
-  ),
-  is_true())
+  curl_vec <-
+    sapply(
+      sapply(
+        vapply(list.files(
+          system.file("extdata",package="curlconverter"),
+          full.names=TRUE, pattern = "curl"
+        ),
+        readLines, character(1), warn=FALSE
+        ),
+        straighten
+      ),
+      length
+    )
+  names(curl_vec) <- basename(names(curl_vec))
+  test_vec <- structure(c(8L, 6L, 6L, 6L, 4L, 5L, 6L, 5L, 6L, 6L),
+                        .Names = c("curl10.txt", "curl1.txt", "curl2.txt",
+                                   "curl3.txt", "curl4.txt", "curl5.txt",
+                                   "curl6.txt", "curl7.txt", "curl8.txt",
+                                   "curl9.txt")
+  )
+  chk <- vapply(names(test_vec), function(x){
+    test_vec[x] == curl_vec[x]
+  }, logical(1))
+  expect_that(all(chk), is_true())
 
 })
 
