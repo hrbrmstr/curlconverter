eb89fc9b3b5808aaa57faa543caaaeba263b8c1b
johndharrison <johndharrison0@gmail.com>
2016-10-19 04:11:13 -0700

    Test output from_har can be straightened

--- tests/testthat/test-from_har.R
+++ tests/testthat/test-from_har.R
@@ -6,3 +6,14 @@ test_that("from_har works", {
   expect_true(inherits(curl_vec, "character"))
 }
 )
+
+test_that("from_har can be straightened", {
+  hardir <- system.file("extdata", package = "curlconverter")
+  harfile <- file.path(hardir, "har1.har")
+  curl_vec <- harfile %>%
+    from_har %>%
+    straighten
+  chk <- vapply(curl_vec, is, logical(1), class2 = "cc_obj")
+  expect_true(all(chk))
+}
+)
