45e054ab1b99fcfc537115dbaddbeaef1f68bfb7
johndharrison <johndharrison0@gmail.com>
2016-10-19 04:21:50 -0700

    Test can straighten and make_req from_har

--- tests/testthat/test-from_har.R
+++ tests/testthat/test-from_har.R
@@ -12,8 +12,20 @@ test_that("from_har can be straightened", {
   harfile <- file.path(hardir, "har1.har")
   curl_vec <- harfile %>%
     from_har %>%
-    straighten
+    straighten(quiet = TRUE)
   chk <- vapply(curl_vec, is, logical(1), class2 = "cc_obj")
+  expect_true(all(chk))
+}
+)
+
+test_that("from_har can make_req", {
+  hardir <- system.file("extdata", package = "curlconverter")
+  harfile <- file.path(hardir, "har1.har")
+  curl_vec <- harfile %>%
+    from_har %>%
+    straighten(quiet = TRUE) %>%
+    make_req
+  chk <- vapply(curl_vec, is, logical(1), class2 = "function")
   expect_true(all(chk))
 }
 )
